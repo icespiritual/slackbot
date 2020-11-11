942fe3ca110d06e6fa5a1d8c360f64c278db266b
Glitch (hello-express) <none>
2020-11-11 09:09:09 +0000

    🛢✂️ Checkpoint ./features/sample_echo.js:23579770/1099

--- features/sample_echo.js
+++ features/sample_echo.js
@@ -7,24 +7,13 @@ module.exports = function(controller) {
 
     controller.hears('宋江','message', async(bot, message) => {
       await bot.reply(message, '志杰.');
-      var google = require('google')
+      const  googleIt = require('google-it')
  
-      google.resultsPerPage = 25
-      var nextCounter = 0
-
-      google('node.js best practices', function (err, res){
-        if (err) console.log("error!")//console.error(err)
-        console.log(res.links.length)
-        for (var i = 0; i < res.links.length; ++i) {
-          var link = res.links[i];
-          console.log(link.title + ' - ' + link.href)
-          console.log(link.description + "\n")
-        }
-
-        if (nextCounter < 4) {
-          nextCounter += 1
-          if (res.next) res.next()
-        }
+      googleIt({'query': 'covfefe irony'}).then(results => {
+        // access to results object here
+        console.log(results)
+      }).catch(e => {
+        console.log("google error")
       })
     });
 
