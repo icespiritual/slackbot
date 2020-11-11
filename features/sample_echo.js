13e45c94caea9995cced302893688e8b8db5d012
Glitch (hello-express) <none>
2020-11-11 17:18:45 +0000

    🌆👾 Checkpoint ./package.json:23579770/975 ./features/sample_echo.js:23579770/801

--- features/sample_echo.js
+++ features/sample_echo.js
@@ -8,21 +8,12 @@ module.exports = function(controller) {
     controller.hears('宋江','message', async(bot, message) => {
       var key = "AIzaSyCXOj-eYdjWCYP4i1FBoEHZj3gNAJovCDY";                // API KEY
       var id = "7c84da9a39b231c0d"; // CSE ID
-      var q = "cats";                          // QUERY
+      const imageSearch = require('image-search-google');
 
-
-      function hndlr(response) {
-        console.log(response);                 // a way to see your results
-      }
-
-      function triggersearch(){
-        var query=document.getElementById("query").value;
-        var JSElement = document.createElement('script');
-        JSElement.src = `https://www.googleapis.com/customsearch/v1?key=${key}&cx=${id}&q=${q}`+query+'&callback=hndlr';
-        document.getElementsByTagName('head')[0].appendChild(JSElement);
-      }
-
-      triggersearch();
+      const client = new imageSearch(id, key);
+      const options = {page:1};
+      var result = await client.search('1234');
+      console.log(result, options)
     });
 
     controller.on('message', async(bot, message) => {
