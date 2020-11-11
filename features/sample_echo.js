562184d42d2ca2fff099941beab48a1825b6132a
Glitch (hello-express) <none>
2020-11-11 16:55:48 +0000

    👋🏘 Checkpoint ./features/sample_echo.js:23579770/1235 ./package.json:23579770/916

--- features/sample_echo.js
+++ features/sample_echo.js
@@ -6,15 +6,23 @@
 module.exports = function(controller) {
 
     controller.hears('宋江','message', async(bot, message) => {
-      await bot.reply(message, '志杰.');
-      const  googleIt = require('google-it')
- 
-      googleIt({'query': 'covfefe irony'}).then(results => {
-        // access to results object here
-        console.log(results)
-      }).catch(e => {
-        console.log("google error")
-      })
+      var key = "AIzaSyCXOj-eYdjWCYP4i1FBoEHZj3gNAJovCDY";                // API KEY
+      var id = "7c84da9a39b231c0d"; // CSE ID
+      var q = "cats";                          // QUERY
+
+
+      function hndlr(response) {
+        console.log(response);                 // a way to see your results
+      }
+
+      function triggersearch(){
+        var query=document.getElementById("query").value;
+        var JSElement = document.createElement('script');
+        JSElement.src = `https://www.googleapis.com/customsearch/v1?key=${key}&cx=${id}&q=${q}`+query+'&callback=hndlr';
+        document.getElementsByTagName('head')[0].appendChild(JSElement);
+      }
+
+      triggersearch();
     });
 
     controller.on('message', async(bot, message) => {
