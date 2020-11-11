6ed9958531e0143ff1aa2c96f6ab045cbde9957a
Glitch (hello-express) <none>
2020-11-11 08:46:08 +0000

    🍄🍍 Checkpoint ./features/sample_echo.js:23579770/934

--- features/sample_echo.js
+++ features/sample_echo.js
@@ -5,9 +5,28 @@
 
 module.exports = function(controller) {
 
-    //controller.hears('宋江','message', async(bot, message) => {
-      //  await bot.reply(message, '志杰.');
-    //});
+    controller.hears('宋江','message', async(bot, message) => {
+      await bot.reply(message, '志杰.');
+      var google = require('google')
+ 
+      google.resultsPerPage = 25
+      var nextCounter = 0
+
+      google('node.js best practices', function (err, res){
+        if (err) console.log("error!")//console.error(err)
+        console.log(res.links.length)
+        for (var i = 0; i < res.links.length; ++i) {
+          var link = res.links[i];
+          console.log(link.title + ' - ' + link.href)
+          console.log(link.description + "\n")
+        }
+
+        if (nextCounter < 4) {
+          nextCounter += 1
+          if (res.next) res.next()
+        }
+      })
+    });
 
     controller.on('message', async(bot, message) => {
         //await bot.reply(message, `Echo: ${ message.text }`);
