d83b751902b59d3a7b586b07516ec93a884d45a7
Glitch (hello-express) <none>
2020-11-13 15:00:12 +0000

    🔦🏪 Checkpoint ./features/sample_echo.js:23579770/5342

--- features/sample_echo.js
+++ features/sample_echo.js
@@ -3,6 +3,42 @@
  * Licensed under the MIT License.
  */
 
+function find_image(bot, message, options, result){
+  if (result.length > 0){
+    console.log(result);
+    var start_value = Math.floor(Math.random() * 5);
+    for (var j=0;j<result.length; j++ ) {
+      var i = (j + start_value)%result.length;
+      if (result[i].url.search('fbsbx') > 0)
+        continue;
+      if (result[i].url.search('.png') > 0 || result[i].url.search('.jpg') > 0 || result[i].url.search('.gif') > 0){
+        console.log(message.text);
+        console.log(result[i].url);
+        /*await bot.reply(message,{
+                blocks: [
+                  {
+                    "type": "image",                 
+                    "image_url": result[i].url,
+                    "alt_text": message.text
+                  },
+                ]
+        });*/
+        bot.reply(message, message.text + '\n' + result[i].url);
+        break;
+      }
+    }
+    // try second page
+    options = {start:11};
+    result = client.search(message.text.slice(1), options);
+
+  }else{
+    if (too_many_request == 1)
+      bot.reply(message, `滿了, 明天請早`);
+    else
+      bot.reply(message, `找不到QQ`);
+  }
+}
+
 module.exports = function(controller) {
 
     controller.hears(new RegExp(/抽/),'message', async(bot, message) => {
@@ -23,32 +59,7 @@ module.exports = function(controller) {
           console.log('search image error' + e);
           too_many_request = 1;
         }
-        if (result.length > 0){
-          console.log(result);
-          var start_value = Math.floor(Math.random() * 5);
-          for (var i=start_value; i<result.length; ++i) {
-            if (result[i].url.search('.png') > 0 || result[i].url.search('.jpg') > 0 || result[i].url.search('.gif') > 0){
-              console.log(message.text);
-              console.log(result[i].url);
-              /*await bot.reply(message,{
-                      blocks: [
-                        {
-                          "type": "image",                 
-                          "image_url": result[i].url,
-                          "alt_text": message.text
-                        },
-                      ]
-              });*/
-              await bot.reply(message, message.text + '\n' + result[i].url);
-              break;
-            }
-          }
-        }else{
-          if (too_many_request == 1)
-            await bot.reply(message, `滿了, 明天請早`);
-          else
-            await bot.reply(message, `找不到QQ`);
-        }
+        find_image(bot, result);
       }
     });
 
