2df70e9340fdcdaee10b86b9b506fc0ab27cb336
Glitch (hello-express) <none>
2020-12-05 15:02:36 +0000

    🎗🛳 Checkpoint ./features/sample_hears.js:23579770/19

--- features/sample_hears.js
+++ features/sample_hears.js
@@ -23,9 +23,9 @@ module.exports = function(controller) {
       console.log(y);
       var kkk = {love:'kkk'};
       await controller.storage.write({ 'superkkk': kkk });
-      //var items = await controller.storage.read('superkkk');
-      //const state = items['superkkk'] || {};
-      console.log(kkk);
+      var items = await controller.storage.read(['superkkk222']);
+      const state = items['superkkk'] || {};
+      console.log(state);
       //var beans = {id: 'cool', beans: 'garbanzo'};
       //controller.storage.write(beans);
       //var newbeans = controller.storage.read('cool');
